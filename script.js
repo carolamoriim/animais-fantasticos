358b80263b61299d4dac3286b0f23781596e069f
Ana Caroline <carolamorim2903@hotmail.com>
2024-02-22 17:49:17 -0400

    modal refatorado com class

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
-import initModal from "./modules/modal.js";
+import Modal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
+import initAnimaNumeros from "./modules/animacao-numeros.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
@@ -19,8 +20,15 @@ accordion.init();
 const tabNav = new TabNav(".js-tabmenu li", ".js-tabcontent section");
 tabNav.init();
 
-initModal();
+const modal = new Modal(
+  '[data-modal="abrir"]',
+  '[data-modal="fechar"]',
+  '[data-modal="container"]'
+);
+modal.init();
+
 initTooltip();
+initAnimaNumeros();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
